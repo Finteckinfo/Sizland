2640ae3feb88811710f19fe3fa99d40998de8501
D3stinn3 <paynedestinne@gmail.com>
2025-04-04 13:24:48 +0300

    About

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -625,20 +625,16 @@ const About = () => {
                         <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
                             <div className="mt-10 lg:mt-0">
                                 <span className="block mb-4 text-lg font-semibold text-dark dark:text-white">
-                                    Why Choose Us
+                                    Why Choose Sizland
                                 </span>
                                 <h2 className="mb-5 text-3xl font-bold text-dark dark:text-white sm:text-[40px]/[48px]">
-                                    Make your customers happy by giving services.
+                                    Built upon state of the art blockchain platforms in a multi chain world!
                                 </h2>
                                 <p className="mb-5 text-base text-body-color dark:text-dark-6">
-                                    It is a long established fact that a reader will be distracted
-                                    by the readable content of a page when looking at its layout.
-                                    The point of using Lorem Ipsum is that it has a more-or-less.
+                                    Sizalnd provisions the best out of any web3 news, first hand interactions with multi chain realm - Sizland
                                 </p>
                                 <p className="mb-8 text-base text-body-color dark:text-dark-6">
-                                    A domain name is one of the first steps to establishing your
-                                    brand. Secure a consistent brand image with a domain name that
-                                    matches your business.
+                                    Sizland is an ecosystem that onboards companies and individual for inclusion!
                                 </p>
                                 <Button1
                                     onClick={() => console.log("Read More clicked")}
